362ce5ce4b90de095fecf75edd0f3e3c6c3f405f
rajat0521 <101493838+rajat0521@users.noreply.github.com>
2022-07-10 16:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ const customMware=require('./config/middleware');
 
 //setup the chat server to be used with socket.io
 //socket.io is used to make chatting engine 
-const chatServer=require('http').Server(app);
+const chatServer=require('http').createServer(app);
 const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
-// chatServer.listen(port2);
+chatServer.listen( process.env.PORT || 3000 );
 // console.log('chat server is listening on port 5000');
 
 const path=require('path');
