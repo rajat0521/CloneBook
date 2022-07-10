c701bca2f42675d4b481c496e05853fd2c7a1b96
rajat0521 <101493838+rajat0521@users.noreply.github.com>
2022-07-10 17:10:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -131,10 +131,10 @@ app.use(customMware.setFlash);
 app.use('/', require('./routes'));
 
 
-app.listen(port, function(err){
-    if (err){
+// app.listen(port, function(err){
+//     if (err){
         // console.log(`Error in running the server: ${err}`);
-    }
+//     }
 
     // console.log(`Server is running on port: ${port}`);
-});
+// });
